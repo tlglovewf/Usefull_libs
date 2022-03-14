0c99f0fd3755d9caa33649ec2f273d1c34767bbc
涂理根04779 <tuligen@navinfo.com>
2022-03-14 17:50:03 +0800

    update

--- examples/Test_DynImport/include/test_Interface.h
+++ examples/Test_DynImport/include/test_Interface.h
@@ -36,11 +36,37 @@ namespace MyInterface
 		virtual void discript()const {};
 	};
 
+	//! 工厂类 主要解决需要多线程的对象问题
+	class IBaseFactory
+	{
+	public:
+		/*
+		 * 构造函数
+		 */
+		virtual std::shared_ptr<IBase> create() = 0;
+	};
 	//class IBase  注册类
 R_REGISTER(IBase)
 #define REGIST_BASE(CLS)	REGIST_INSTANCE(CLS,	IBase)
 #define UNREGIST_BASE(CLS)	UNREGIST_INSTANCE(CLS,	MyInterface::R_IBase)
 #define GET_BASE(CLS)		GET_INSTANCE(CLS,		MyInterface::R_IBase)
 
+
+/*
+ * 工厂注册(参考)
+ */
+#define REGIST_DBASE(CLS) \
+			class CLS##Factory : public IBaseFactory\
+			{\
+			public:	\
+				virtual std::shared_ptr<IBase> create()\
+				{ \
+					return std::make_shared<CLS>();\
+				}\
+			};\
+		REGIST_INSTANCE(CLS##Factory,IBase);
+
+#define CREATE_BASE(CLS) \
+		GET_BASE(CLS)->create();
 #pragma endregion 
 }
