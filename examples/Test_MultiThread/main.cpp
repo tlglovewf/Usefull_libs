c2e4a4e86948b4cf70bc40f4bb7190b7ebe35ead
涂理根04779 <tuligen@navinfo.com>
2022-04-19 14:36:21 +0800

    update

--- examples/Test_MultiThread/main.cpp
+++ examples/Test_MultiThread/main.cpp
@@ -35,7 +35,7 @@ void testSeq()
 	}
 }
 
-int main(int argc, char** argv)
+void test_atomic()
 {
 	clock_t start_time = clock();
 
@@ -56,5 +56,70 @@ int main(int argc, char** argv)
 
 	cout << "cost time : " << end_time - start_time << "ms" << endl;
 	//结果来看 原子操作的共享资源效率 提高了至少一倍
+}
+
+
+void callback()
+{
+	std::cout << "function finish!!!" << std::endl;
+}
+
+#include <functional>
+#include <future>
+void dosomething(std::function<void()> func)
+{
+	std::cout << "hello world" << std::endl;
+
+	func();
+}
+
+class Te
+{
+public:
+	void callback()
+	{
+		std::cout << "member callback ." << std::endl;
+	}
+};
+
+
+void testcallback()
+{
+	//package 可以理解为一个函数包   
+	std::packaged_task<void()> foo(callback);
+	auto ft = foo.get_future();
+	//可以直接与thread绑定
+	std::thread(std::move(foo)).detach();
+	std::cout << "!" << " " << ft.valid() << std::endl;
+	ft.get();
+	//可以判断有效性判断线程是否执行完毕(前提是线程能正常执行完毕)
+	std::cout << "!" << " " << ft.valid() << std::endl;
+	
+	static Te te;
+	std::thread  th(dosomething, std::bind(&Te::callback ,&te));
+	th.detach();
+
+
+	std::promise<int>  prm;
+	auto print_promise = [&prm]()
+	{
+		auto fut = prm.get_future();
+		std::cout << "value is : " << fut.get() << std::endl;
+	};
+
+	thread thr(print_promise);
+	//set_value 只能执行一次
+	prm.set_value(10);
+
+	thr.join();
+}
+#include <chrono>
+int main(int argc, char** argv)
+{
+	//test_atomic();
+
+	testcallback();
+
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 	return 0;
 }
